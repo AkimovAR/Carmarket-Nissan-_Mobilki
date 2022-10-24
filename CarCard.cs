89cab9e04e9cd8043508176e5c90ac2c048f3474
AkimovAR <93865351+AkimovAR@users.noreply.github.com>
2022-10-24 22:38:54 +0300

    Add files via upload

---
+++ CarCard.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App1
+{
+    public class CarCard
+    {
+        /*Класс с публичными строковыми свойствами*/
+        public string Name { get; set; }
+
+        public string Price { get; set; }
+
+        public string Probeg { get; set; }
+
+        public string Toplivo { get; set; }
+
+        public string KPPType { get; set; }
+
+        public string Color { get; set; }
+
+        public string Privod { get; set; }
+
+        public string Kuzov { get; set; }
+
+        public string Image { get; set; }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+
+
+    public partial class MainPage : ContentPage
+    {
+        /*Коллекция типа IList для хранения объектов CarCard*/
+        public IList<CarCard> CarCards { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing() /*Метод заполнения коллекции*/
+        {
+            CarCards = new List<CarCard>(); /*Ссылка на экземпляр коллекции*/
+
+            CarCards.Add(new CarCard() /*заполнение методом Add()*/
+            {
+                Name = "Nissan Qashqai I, 2007",
+                Price = "от 549 000 Р",
+                Probeg = "173 919 км",
+                Toplivo = "Бензин",
+                KPPType = "Вариатор",
+                Color = "Чёрный",
+                Privod = "Передний",
+                Kuzov = "5 дв.",
+                Image = "https://carmetrika.ru/images/items/a68/a192b3a2a/b177b40ceb9c.jpg"
+            });
+
+            CarCards.Add(new CarCard()
+            {
+                Name = "Nissan Juke I, 2012",
+                Price = "от 890 000 Р",
+                Probeg = "197 332 км",
+                Toplivo = "Бензин",
+                KPPType = "Вариатор",
+                Color = "Красный",
+                Privod = "Передний",
+                Kuzov = "5 дв.",
+                Image = "https://avatars.mds.yandex.net/get-autoru-vos/2171556/f187db5581637260968758d565b9aa38/1200x900"
+            });
+
+            CarCards.Add(new CarCard()
+            {
+                Name = "Nissan Almera III, 2017",
+                Price = "от 620 000 Р",
+                Probeg = "101 948 км",
+                Toplivo = "Бензин",
+                KPPType = "Механика",
+                Color = "Чёрный",
+                Privod = "Передний",
+                Kuzov = "Седан",
+                Image = "https://avatars.mds.yandex.net/get-autoru-vos/1870907/e25825505fd93d8cd4c732fa72b02bcd/1200x900"
+            });
+
+            CarCards.Add(new CarCard()
+            {
+                Name = "Nissan Almera III, 2017",
+                Price = "от 620 001 Р",
+                Probeg = "101 948 км",
+                Toplivo = "Бензин",
+                KPPType = "Механика",
+                Color = "Чёрный",
+                Privod = "Передний",
+                Kuzov = "Седан",
+                Image = "https://avatars.mds.yandex.net/get-autoru-vos/2172360/49c64715db9eb7bd206c5c4f18b5641f/1200x900"
+            });
+
+            BindingContext = this; /*Ссылка на текущий экземпляр класса*/
+
+            base.OnAppearing();
+        }
+    }
+}
